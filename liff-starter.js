a160ec2c8a92afec0d1577219c38bd8f7d51d62c
ParazitTR <73781102+ParazitTR@users.noreply.github.com>
2021-04-18 13:44:08 +0300

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -128,7 +128,7 @@ function registerButtonHandlers() {
         } else {
             liff.sendMessages([{
                 'type': 'text',
-                'text': "You've successfully sent a message! Hooray!"
+                'text': "EposTeam Liff ine girmiş bulunuyorsunuz"
             }]).then(function() {
                 window.alert('Message sent');
             }).catch(function(error) {
